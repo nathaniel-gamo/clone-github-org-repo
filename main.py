9a8a9733dcd0035fa38dbc211d12a22be816fc0c
Nathaniel Gamo <nathaniel.c.gamo@gmail.com>
2025-04-21 05:27:09 +0800

    updated function

--- main.py
+++ main.py
@@ -5,10 +5,8 @@ from git import Repo
 import src.config as config
 
 
-def download_repo() -> None:
-    logging.info("cloning...")
-    repo_url: str = "https://github.com/ncgmyorg/sample.git"
-    destination_directory: str = r"C:\Users\Nathaniel Gamo\Documents\Python Projects\sample"
+def clone_repo(repo_url: str , destination_directory: str) -> None:
+    logging.info(f"Cloning {repo_url}")
     Repo.clone_from(repo_url, destination_directory)
     
 def main() -> None:
@@ -19,7 +17,10 @@ def main() -> None:
         filename=f"{config.LOG_FILE_NAME}"
     )
 
-    download_repo()
+    repo_url: str = "https://github.com/ncgmyorg/sample.git"
+    destination_directory: str = r"C:\Users\Nathaniel Gamo\Documents\Python Projects\sample"
+
+    clone_repo(repo_url, destination_directory)
 
 if __name__ == "__main__":
     main()

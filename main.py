f4f0cdf788fca68f69375cb4546665c438d857cd
Nathaniel Gamo <nathaniel.c.gamo@gmail.com>
2025-05-07 19:47:41 +0800

    fix mypy issues

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import src.config as config
 
 def clone_repo(repo_url: str , destination_directory: str) -> None:
     logging.info(f"Cloning {repo_url}...")
+
     try:
         Repo.clone_from(repo_url, destination_directory)
     except Exception as e:
@@ -18,21 +19,26 @@ def pull_repo(repo_path: str,
               remote: str = "origin", 
               branch: str = "main") -> None:
     logging.info(f"Pulling {repo_path}...")
+
     try:
         Repo(repo_path).remotes[remote].pull(branch)
     except Exception as e:
         logging.error(f"Failed to pull. Error {e}.")
 
-def get_org_repo_urls(org_name: str, token: str) -> list[str]:
+def get_org_repo_urls(org_name: str, token: str) -> list[str] | None:
     logging.info("Getting repository URLs...")
 
-    g: Github = Github(token)
-    org: Organization.Organization = g.get_organization(org_name)
-    
-    repo: Repository.Repository
-    repo_urls: list[str] = [repo.html_url for repo in org.get_repos()]
+    try:
+        g: Github = Github(token)
+        org: Organization.Organization = g.get_organization(org_name)
+        
+        repo: Repository.Repository
+        repo_urls: list[str] = [repo.html_url for repo in org.get_repos()]
 
-    return repo_urls
+        return repo_urls
+    except Exception as e:
+        logging.error(f"Failed to get repository URLs. Error {e}.")
+        return None
     
 def main() -> None:
     logging.basicConfig(
@@ -49,21 +55,24 @@ def main() -> None:
                                             "Documents", 
                                             "Python Projects")
 
-        repo_urls: list[str] = get_org_repo_urls(config.GITHUB_ORG_NAME, 
-                                                 config.GITHUB_TOKEN)
+        if config.GITHUB_ORG_NAME is not None and config.GITHUB_TOKEN is not None:
+            repo_urls: list[str] | None = get_org_repo_urls(config.GITHUB_ORG_NAME, 
+                                                            config.GITHUB_TOKEN)
 
         repo_url: str
-        for repo_url in repo_urls:
-            destination_folder: str = repo_url.split("/")[-1]
 
-            destination_directory: str = os.path.join(local_directory, 
-                                                    destination_folder)
+        if repo_urls is not None:
+            for repo_url in repo_urls:
+                destination_folder: str = repo_url.split("/")[-1]
 
-            if os.path.exists(destination_directory):
-                pull_repo(destination_directory)
-            else:
-                os.makedirs(destination_directory)
-                clone_repo(repo_url, destination_directory)
+                destination_directory: str = os.path.join(local_directory, 
+                                                        destination_folder)
+
+                if os.path.exists(destination_directory):
+                    pull_repo(destination_directory)
+                else:
+                    os.makedirs(destination_directory)
+                    clone_repo(repo_url, destination_directory)
 
 if __name__ == "__main__":
     main()

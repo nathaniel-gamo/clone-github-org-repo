d668cd416723130bdb878f59d9812320bc44c114
Nathaniel Gamo <nathaniel.c.gamo@gmail.com>
2025-05-07 19:38:48 +0800

    added logic for getting repo url of an org, installed PyGithub

--- main.py
+++ main.py
@@ -2,22 +2,37 @@ import logging
 import os
 
 from git import Repo
+from github import Github, Organization, Repository
 
 import src.config as config
 
 
 def clone_repo(repo_url: str , destination_directory: str) -> None:
-    logging.info(f"Cloning {repo_url}")
-    Repo.clone_from(repo_url, destination_directory)
+    logging.info(f"Cloning {repo_url}...")
+    try:
+        Repo.clone_from(repo_url, destination_directory)
+    except Exception as e:
+        logging.error(f"Failed to clone. Error {e}.")
 
 def pull_repo(repo_path: str, 
               remote: str = "origin", 
               branch: str = "main") -> None:
-    logging.info(f"Pulling {repo_path}")
+    logging.info(f"Pulling {repo_path}...")
     try:
         Repo(repo_path).remotes[remote].pull(branch)
     except Exception as e:
         logging.error(f"Failed to pull. Error {e}.")
+
+def get_org_repo_urls(org_name: str, token: str) -> list[str]:
+    logging.info("Getting repository URLs...")
+
+    g: Github = Github(token)
+    org: Organization.Organization = g.get_organization(org_name)
+    
+    repo: Repository.Repository
+    repo_urls: list[str] = [repo.html_url for repo in org.get_repos()]
+
+    return repo_urls
     
 def main() -> None:
     logging.basicConfig(
@@ -27,16 +42,28 @@ def main() -> None:
         filename=f"{config.LOG_FILE_NAME}"
     )
 
-    repo_url: str = "https://github.com/ncgmyorg/sample.git"
-    destination_folder: str = repo_url.split("/")[-1].replace(".git", "")
-    local_directory: str = os.path.join(os.environ.get("USERPROFILE"), "Documents", "Python Projects")
-    destination_directory: str = os.path.join(local_directory, destination_folder)
+    userprofile: str | None = os.environ.get("USERPROFILE")
 
-    if os.path.exists(destination_directory):
-        pull_repo(destination_directory)
-    else:
-        os.makedirs(destination_directory)
-        clone_repo(repo_url, destination_directory)
+    if userprofile:
+        local_directory: str = os.path.join(userprofile, 
+                                            "Documents", 
+                                            "Python Projects")
+
+        repo_urls: list[str] = get_org_repo_urls(config.GITHUB_ORG_NAME, 
+                                                 config.GITHUB_TOKEN)
+
+        repo_url: str
+        for repo_url in repo_urls:
+            destination_folder: str = repo_url.split("/")[-1]
+
+            destination_directory: str = os.path.join(local_directory, 
+                                                    destination_folder)
+
+            if os.path.exists(destination_directory):
+                pull_repo(destination_directory)
+            else:
+                os.makedirs(destination_directory)
+                clone_repo(repo_url, destination_directory)
 
 if __name__ == "__main__":
     main()
--- src/config.py
+++ src/config.py
@@ -5,3 +5,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 LOG_FILE_NAME: str | None = os.environ.get("LOG_FILE_NAME")
+GITHUB_ORG_NAME: str | None = os.environ.get("GITHUB_ORG_NAME")
+GITHUB_TOKEN: str | None = os.environ.get("GITHUB_TOKEN")

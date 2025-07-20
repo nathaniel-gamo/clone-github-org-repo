35fe869144befe3554cec1dce06cffe5d0ab1b08
Nathaniel Gamo <nathaniel.c.gamo@gmail.com>
2025-07-20 23:01:57 +0800

    updated code

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def clone_repo(repo_url: str , destination_directory: str) -> None:
     try:
         Repo.clone_from(repo_url, destination_directory)
     except Exception as e:
-        logging.error(f"Failed to clone. Error {e}.")
+        logging.error(f"Failed to clone.\nError message: {e}")
 
 def pull_repo(repo_path: str, 
               remote: str = "origin", 
@@ -24,50 +24,58 @@ def pull_repo(repo_path: str,
     try:
         Repo(repo_path).remotes[remote].pull(branch)
     except Exception as e:
-        logging.error(f"Failed to pull. Error {e}.")
+        logging.error(f"Failed to pull.\nError message: {e}")
 
-def get_org_repo_urls(org_name: str, token: str) -> list[str] | None:
+def get_org_repo_urls(org: str, token: str) -> list[str] | None:
     logging.info("Getting repository URLs...")
 
     try:
-        g: Github = Github(token)
-        org: Organization.Organization = g.get_organization(org_name)
+        github: Github = Github(token)
+        github_org: Organization.Organization = github.get_organization(org)
         
         repo: Repository.Repository
-        repo_urls: list[str] = [repo.html_url for repo in org.get_repos()]
+        repo_urls: list[str] = [repo.html_url for repo in 
+                                github_org.get_repos()]
 
         return repo_urls
     except Exception as e:
-        logging.error(f"Failed to get repository URLs. Error {e}.")
+        logging.error(f"Failed to get repository URLs.\nError message: {e}")
         return None
     
 def main(args: argparse.Namespace) -> None:
-    userprofile: str | None = os.environ.get("USERPROFILE")
+    try:
+        logging.info("Executing main.py...")
 
-    if userprofile:
-        local_directory: str = os.path.join(userprofile, 
-                                            "Documents", 
-                                            "Python Projects")
+        if args.github_org is None or args.github_token is None:
+            raise ValueError("GitHub Organization Name "
+                             "or GitHub Token is empty.")
 
-        if (args.github_org_name is not None 
-            and args.github_token is not None):
+        userprofile: str | None
+        if userprofile:= os.environ.get("USERPROFILE"):
+            local_directory: str = os.path.join(userprofile, 
+                                           "Documents", 
+                                            config.DESTINATION_FOLDER_NAME)
+            
             repo_urls: list[str] | None = get_org_repo_urls(
-                args.github_org_name, 
-                args.github_token)
+                args.github_org, args.github_token)
 
-        if repo_urls is not None:
-            repo_url: str
-            for repo_url in repo_urls:
-                destination_folder: str = repo_url.split("/")[-1]
-                
-                destination_directory: str = os.path.join(local_directory, 
-                                                        destination_folder)
+            if repo_urls is not None:
+                repo_url: str
+                for repo_url in repo_urls:
+                    destination_folder: str = repo_url.split("/")[-1]
+                    
+                    destination_directory: str = os.path.join(
+                        local_directory, destination_folder)
 
-                if os.path.exists(destination_directory):
-                    pull_repo(destination_directory)
-                else:
-                    os.makedirs(destination_directory)
-                    clone_repo(repo_url, destination_directory)
+                    if os.path.exists(destination_directory):
+                        pull_repo(destination_directory)
+                    else:
+                        os.makedirs(destination_directory)
+                        clone_repo(repo_url, destination_directory)
+        
+    except Exception as e:
+        logging.error(f"An error occurred during the "
+                      f"execution of main.py.\nError message: {e}")
 
 if __name__ == "__main__":
     logging.basicConfig(
@@ -80,14 +88,18 @@ if __name__ == "__main__":
     parser: argparse.ArgumentParser = argparse.ArgumentParser(
         description="download-github-repositories")
     
-    parser.add_argument("github_org_name", 
+    parser.add_argument("github_org", 
+                        default=None, 
+                        nargs="?", 
                         type=str, 
-                        help="GitHub Organization")
+                        help="GitHub Organization"
+                        )
     
     parser.add_argument("github_token", 
-                        type=str, 
+                        default=None, 
+                        nargs="?", 
+                        type=str , 
                         help="GitHub Token")
-    
     
     args: argparse.Namespace = parser.parse_args()
 

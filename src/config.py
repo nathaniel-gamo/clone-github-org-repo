b113f86f4cf44224593b7982124248650ba9cb98
Nathaniel Gamo <nathaniel.c.gamo@gmail.com>
2026-08-24 05:23:58 +0800

    updated validation of .env

--- src/config.py
+++ src/config.py
@@ -8,23 +8,25 @@ from dotenv import dotenv_values, load_dotenv
 def _get_dotenv_path() -> str:
 
     base_dir: Path
-    if getattr(sys, "frozen", False) or "__compiled__" in globals():
-        base_dir = Path(sys.executable).parent
 
+    if getattr(sys, "frozen", False):
+        base_dir = Path(sys.executable).parent
     else:
-        base_dir = Path(__file__).parent.parent
-    
+        base_dir = Path(sys.argv[0]).parent
+
     return str(base_dir / ".env")
 
 
-def load_config(dotenv_path: str = "", 
-                dotenv_only: bool = True) -> dict[str, str | None]:
-    
-    dotenv_path = (dotenv_path 
-                   if (Path(dotenv_path).is_file() 
-                       and dotenv_path[-4:] == ".env")
-                   else _get_dotenv_path())
-    
+def load_config(
+        dotenv_path: str = "", 
+        dotenv_only: bool = True) -> dict[str, str | None]:
+
+    dotenv_path = (
+        dotenv_path
+        if Path(dotenv_path).is_file() and Path(dotenv_path).name == ".env"
+        else _get_dotenv_path()
+    )
+
     if dotenv_only:
         return dotenv_values(dotenv_path)
 
